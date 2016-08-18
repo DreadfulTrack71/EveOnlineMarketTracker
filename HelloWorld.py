a7f70fad87d2e87777ef40da2d879c374f878232
jtp92 <jperry4192@gmail.com>
2016-08-18 08:42:31 -0400

    Added HelloWorld.py for testing as well as some text files for project planning

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+# A simple HelloWorld program for testing purposes
+# Author: Jonathan Perry
+# Date: 2016-08-18
+# Version: 1.0
+
+def main():
+    print("Hello, World!")
+    printText()
+
+def printText():
+    print("This is some text!")
+
+if __name__ == "__main__": main()
